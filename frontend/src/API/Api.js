3414f2a991c324b534604fcbb260b0f9eeb022c2
d3nnysm2904 <dennymosquera@outlook.com>
2023-03-28 01:28:12 -0400

    api

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -12,29 +12,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class JoblyApi
 {
-  // the token for interactive with the API will be stored here.
-  static token;
 
-  static async request ( endpoint, data = {}, method = "get" )
-  {
-    console.debug( "API Call:", endpoint, data, method );
-
-    const url = `${ BASE_URL }/${ endpoint }`;
-    const headers = { Authorization: `Bearer ${ JoblyApi.token }` };
-    const params = ( method === "get" )
-      ? data
-      : {};
-
-    try
-    {
-      return ( await axios( { url, method, data, params, headers } ) ).data;
-    } catch ( err )
-    {
-      console.error( "API Error:", err.response );
-      let message = err.response.data.error.message;
-      throw Array.isArray( message ) ? message : [ message ];
-    }
-  }
 
   // Individual API routes
 
