66e3189e3ba35682fd76519d7a92e1417f845161
d3nnysm2904 <dennymosquera@outlook.com>
2023-03-27 23:59:38 -0400

    config

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, function () {
-  console.log(`Started on http://localhost:${PORT}`);
-});
+// app.listen(PORT, function () {
+//   console.log(`Started on http://localhost:${PORT}`);
+// });
+app.listen( PORT, '0.0.0.0', () =>
+{
+  console.log( `Server Started at Port ${ PORT }` );
+} );
