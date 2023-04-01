d7045a465ed0169fe181c074e77f122fd5b27931
d3nnysm2904 <dennymosquera@outlook.com>
2023-04-01 11:18:02 -0400

    apa

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "harmonious-degree-production.up.railway.app";
+const BASE_URL = process.env.REACT_APP_BASE_URL 
 
 /** API Class.
  *
